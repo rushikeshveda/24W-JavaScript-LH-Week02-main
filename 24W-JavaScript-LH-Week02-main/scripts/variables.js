3149ad48d5ffec114b0b31f258f17201d389c4f4
Mona Lisa <rushikeshveda@gmail.com>
2024-01-16 19:01:53 -0500

    first commit

---
+++ 24W-JavaScript-LH-Week02-main/scripts/variables.js
@@ -0,0 +1,27 @@
+/* STEP 1: Declare variables
+	Use a-z, 0-9, A-Z and underscores, avoiding reserved names
+	All program variables are generally declared at the beginning (called 'hoisting') */
+var myName;
+var myAge;
+
+
+   
+    /* STEP 2: Initialize variables
+	Don't confuse a variable with no value with one that doesn't exist at all */
+ myName = "Rushikesh";
+ console.log(myName)
+    /* STEP 3: Updating variables
+	Once a variable has been declared, this can be done any time */
+
+    // STEP 4: Variable types
+    // Numbers
+
+    // Strings (use '' or "")
+
+    // Booleans
+
+    // Arrays
+
+    // Objects
+
+    // STEP 5: Typing (JavaScript is a loosely-typed language)
